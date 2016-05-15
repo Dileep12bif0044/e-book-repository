33d4ecd47cb17841cb2b7f4448770ee110fb5003
dileep/dlp <dileepk025@gmail.com>
2016-05-15 15:16:50 +0530

    first commit

---
+++ e-bookCodes/signupp.php
@@ -0,0 +1,30 @@
+	<?php
+
+	define('DB_NAME','bey');
+	define('DB_USER','root');
+	define('DB_PASSWORD','');
+	define('DB_HOST','localhost');
+	
+	$link=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
+	
+	$db_selected=mysql_select_db(DB_NAME,$link);
+	
+	
+	
+	$val1=$_POST['name'];
+	$val2=$_POST['phone'];
+	$val3=$_POST['email'];
+	$val4=$_POST['password'];
+	
+	
+	$sql="INSERT INTO checkt (name,phone,email,password) VALUES ('$val1','$val2','$val3','$val4')";
+	if(!mysql_query($sql)) {
+		echo "Already user exits";
+	}
+	else {
+		echo "data entered successfuly";
+		header("Location: zoo.html");
+	}
+	
+	mysql_close();
+	?>
